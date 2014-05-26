fd125a9d93e092855177e4fce7193cc54f0eef31
Milinda Perera <coding@milinda-perera.com>
2014-05-26 00:43:06 -0700

    - cleaned utils_poly.py

--- utils_poly.py
+++ utils_poly.py
@@ -1,7 +1,5 @@
 __author__ = 'Milinda Perera'
 
-from charm.toolbox.integergroup import integer
-
 
 def poly_from_roots(roots, neg_one, one):
     """
@@ -75,23 +73,3 @@ def poly_coef_to_str(coef, degree):
     elif degree > 1:
         out.append('X^' + str(degree))
     return out
-
-
-def test_poly():
-    coefs1 = map(integer, [-1, -2])
-    coefs2 = [3, 4]
-    coefs3 = poly_mul(coefs1, coefs2, 0)
-    poly_print(coefs1)
-
-    roots = [2, 3, 4, 5]
-    roots = map(integer, roots)
-    coefs = poly_from_roots(roots, -1, 1)
-    eval1 = poly_eval(coefs, 3)
-    eval2 = poly_eval_horner(coefs, 3)
-    poly_print(coefs)
-    print(eval1)
-    print(eval2)
-
-
-if __name__ == '__main__':
-    test_poly()
